100e810b2cf265e66eb0484b49f236c44c2d99aa
Dibyanshu Shekhar Dey <66998557+dibyanshushekhardey@users.noreply.github.com>
2022-07-14 18:15:42 +0530

    Delete Web_Dev/oldPortfolios/deploying_internshala_assignment directory

--- Web_Dev/oldPortfolios/deploying_internshala_assignment/includes/common.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $conn = mysqli_connect("localhost", "root", "", "store") or die(mysqli_error($conn));
-    
-    if(!isset($_SESSION)){
-        session_start();
-    }
-?>
--- Web_Dev/oldPortfolios/deploying_internshala_assignment/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-	<div class="container">
-		Copyright <span class="glyphicon glyphicon-copyright-mark"></span> LifeStyle Stores.
-		All Rights Reserved | Contact Us: +91 90000 00000
-	</div>
-</footer>
